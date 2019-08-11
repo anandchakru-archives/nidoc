aadd3b1e9c4abf06114686add36fa5371d1e89fb
Anand C <anandchakru.forum@gmail.com>
2019-08-11 09:43:22 -0700

    buildinfo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AppService } from './services/app.service';
+import { BUILDINFO } from 'src/environments/buildinfo';
 
 @Component({
   selector: 'n3-root',
@@ -7,5 +8,6 @@ import { AppService } from './services/app.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  buildinfo = BUILDINFO;
   constructor(public app: AppService) { }
 }
---
+++ src/environments/buildinfo.ts
@@ -0,0 +1,11 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const BUILDINFO = {
+  "hash": {
+    "short": "db96276",
+    "long": ""
+  },
+  "tag": "",
+  "version": "0.0.0"
+};
+/* tslint:enable */
